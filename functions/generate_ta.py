a9bc2c6c89aa6fd39316a61811dba85eee8169f3
SuperAngryFork <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-09 16:01:52 -0800

    Started the Technical Analysis fields

--- functions/generate_ta.py
+++ functions/generate_ta.py
@@ -1,9 +1,7 @@
-#%%
 import pandas_ta as ta
 
 import generate_db
 
-#%%
 def sp500_rsi(start_date,
               rsi_lt = 30,
               rsi_mt = 15,
@@ -12,12 +10,13 @@ def sp500_rsi(start_date,
     sp500[f'rsi{rsi_lt}'] = ta.rsi(close = sp500.Close, length=rsi_lt)
     sp500[f'rsi{rsi_mt}'] = ta.rsi(close = sp500.Close, length=rsi_mt)
     sp500[f'rsi{rsi_st}'] = ta.rsi(close = sp500.Close, length=rsi_st)
-    return sp500
+    
 
 def sp500_bbands(start_date,
                  bbands_length = 20,
                  bbands_std = 2):
-    
-    my_bbands = ta.bbands(close = sp500_ta.Close, length=bbands_length, std=bbands_std)
-    sp500_ta=sp500_ta.join(my_bbands)
+    sp500 = generate_db.generate_sp500(start_date)
+    my_bbands = ta.bbands(close = sp500.Close, length=bbands_length, std=bbands_std)
+    # maybe look into renaming bbands if necessary
+    sp500 = sp500.join(my_bbands)
     
