acc7f56aeb8b0853b660c120a51aad351508cb92
WaqarQureshii <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-13 22:01:22 -0800

    Fixed ndx and sp500 ratio

--- functions/generate_ta.py
+++ functions/generate_ta.py
@@ -6,16 +6,18 @@ import generate_db
 def nasdaqvssp500(start_date, interval = '1d'):
     nasdaq = generate_db.generate_ndx(start_date, interval=interval)
     nasdaq.drop(['Open', 'High', 'Low', 'Volume', '% Change'], axis = 1, inplace=True)
-    nasdaq.rename(columns)
+    nasdaq.rename(columns={'Close':'Nasdaq Close'}, inplace=True)
 
     sp500 = generate_db.generate_sp500(start_date, interval=interval)
-    # sp500.drop(['Open', 'High', 'Low', 'Volume', '% Change'], axis = 1, inplace=True)
+    sp500.drop(['Open', 'High', 'Low', 'Volume', '% Change'], axis = 1, inplace=True)
+    sp500.rename(columns={'Close':'SP500 Close'}, inplace=True)
+
+    ndxvssp500 = pd.concat([nasdaq, sp500], axis=1)
+    ndxvssp500['Nasdaq vs SP500 Ratio'] = ndxvssp500['Nasdaq Close']/ndxvssp500['SP500 Close']
+    ndxvssp500.drop(['Nasdaq Close', 'SP500 Close'], axis = 1, inplace=True)
     
-    # ndxvssp500 = pd.concat([nasdaq, sp500], axis=1)
-    # ndxvssp500['Nasdaq vs SP500 Ratio'] = ndxvssp500['Nasdaq Close']/ndxvssp500['SP500 Close']
-    # ndxvssp500.drop(['Nasdaq Close', 'SP500 Close', 'SP500 % Change'], axis = 1, inplace=True)
-    
-    return nasdaq
+    return ndxvssp500
+
 
 def sp500_rsi(start_date,
               rsi_lt = 30,
@@ -28,6 +30,7 @@ def sp500_rsi(start_date,
     sp500[f'rsi_{rsi_st}'] = ta.rsi(close = sp500.Close, length=rsi_st)
     return sp500
 
+
 def sp500_bbands(start_date,
                  bbands_length = 20,
                  bbands_std = 2):
@@ -36,6 +39,7 @@ def sp500_bbands(start_date,
     # maybe look into renaming bbands if necessary
     sp500 = sp500.join(my_bbands)
     return sp500
+
 
 def yield_difference(start_date, lt_yield_inp = '30y'):
     #LONG-TERM YIELD GENERATION
