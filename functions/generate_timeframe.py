b03672c9336e750c9be5be95a0c76d83d6054e39
SuperAngryFork <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-14 13:10:20 -0800

    Develop Streamlit infrastructure

---
+++ functions/generate_timeframe.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+import generate_db
+import generate_ta
+
---
+++ functions/last_business_date.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+def last_biz_date():
+    today = datetime.today()
+    last_biz_day = pd.date_range(end=today - timedelta(days=1), periods=1, freq='B')[0].strftime('%Y-%m-%d')
+    return last_biz_day
+
+def today_date():
+    today = datetime.today()
+    last_biz_day = pd.date_range(end=today, periods=1, freq='B')[0].strftime('%Y-%m-%d')
+    return today_date
