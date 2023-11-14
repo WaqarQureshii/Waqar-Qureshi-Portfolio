5928884ed01dfb99ab54b714519457f891b2feec
WaqarQureshii <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-13 22:00:54 -0800

    Include % Changes in database

--- functions/generate_db.py
+++ functions/generate_db.py
@@ -49,42 +49,69 @@ def generate_3mrx(start_date, interval = '1d'):
                        start_date,
                        interval = interval)
     r03m.drop(["Volume", 'Open', 'High', 'Low', 'Adj Close'], axis=1, inplace=True)
+    
+    r03m['% Change'] = r03m['Close'].pct_change() * 100
+
     return r03m
+
 
 def generate_10yrx(start_date, interval = '1d'):
     r10y = yf.download(['^TNX'],
                        start_date,
                        interval = interval)
     r10y.drop(["Volume", 'Open', 'High', 'Low', 'Adj Close'], axis=1, inplace=True)
+    
+    r10y['% Change'] = r10y['Close'].pct_change() * 100
+
     return r10y
+
 
 def generate_30yrx(start_date, interval = '1d'):
     r30y = yf.download(['^TYX'],
                        start_date,
                        interval = interval)
     r30y.drop(["Volume", 'Open', 'High', 'Low', 'Adj Close'], axis=1, inplace=True)
+    
+    r30y['% Change'] = r30y['Close'].pct_change() * 100
+
     return r30y
+
 
 def generate_hyg(start_date, interval = '1d'):
     hyg = yf.download(['HYG'],
                       start_date,
                       interval = interval)
+    
+    hyg['% Change'] = hyg['Close'].pct_change() * 100
+
     return hyg
+
 
 def generate_energy(start_date, interval = '1d'):
     energy = yf.download(['XLE'],
                          start_date,
                          interval = interval)
+    
+    energy['% Change'] = energy['Close'].pct_change() * 100
+    
     return energy
+
 
 def generate_utility(start_date, interval = '1d'):
     utility = yf.download(['XLU'],
                           start_date,
                           interval = interval)
+    
+    utility['% Change'] = utility['Close'].pct_change() * 100
+    
     return utility
+
 
 def generate_consumer(start_date, interval = '1d'):
     consumer = yf.download(['XLY'],
                            start_date,
                            interval = interval)
+    
+    consumer['% Change'] = consumer['Close'].pct_change() * 100
+    
     return consumer
