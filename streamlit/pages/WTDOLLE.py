d28ae6202fdcf81cdc6c97907d8184cbbf1ba975
SuperAngryFork <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-14 16:21:52 -0800

    Trying to import functions

--- streamlit/pages/WTDOLLE.py
+++ streamlit/pages/WTDOLLE.py
@@ -1,13 +1,13 @@
-import streamlit as st
+# import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-from ...functions.generate_db import *
-from ...functions.last_business_date import last_biz_date, today_date
+from functions.generate_db import *
+from functions.last_business_date import *
 
 interval_options = ['1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo']
 
-interval = '1m'
+interval = '1wk'
 start_date = '2008-01-01'
 
 vix = generate_vix(start_date, interval = interval)
