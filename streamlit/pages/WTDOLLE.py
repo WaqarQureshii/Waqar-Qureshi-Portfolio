c3ee8034fc7ef683c919ff675080d0bd215a3b91
WaqarQureshii <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-15 21:02:21 -0800

    Added more options into WTDOLLE

--- streamlit/pages/WTDOLLE.py
+++ streamlit/pages/WTDOLLE.py
@@ -8,17 +8,45 @@ sys.path.append("..")
 from functions.generate_db import generate_vix
 
 
-show_vix = st.checkbox('VIX')
+col1, col2, col3, col4, col5 = st.columns([0.2, 0.2, 0.2, 0.2, 0.2])
 
-if show_vix == True:
-    show_vix_label = st.radio('Choose Signal Type',
-                              ["VIX level", 'VIX % Change'])
-else:
-    pass
+with col1:
+    st.write("Volatility Index")
+    
+    show_vix = st.checkbox('VIX')
+    if show_vix == True:
+        show_vix_label = st.radio('Choose Signal Type',
+                                ["VIX level", 'VIX % Change'])
+    else:
+        pass
 
-show_hyg = st.checkbox('HYG')
-if show_hyg == True:
-    show_hyg_label = st.radio('Choose Signal Type',
-                              ["HYG level", 'HYG % Change'])
-else:
-    pass
+
+with col2:
+    st.write("Junk Bonds")
+    show_hyg = st.checkbox('HYG')
+    if show_hyg == True:
+        show_hyg_label = st.radio('Choose Signal Type',
+                                ["HYG level", 'HYG % Change'])
+    else:
+        pass
+
+with col3:
+    st.write("Overbought / Oversold")
+    show_RSI = st.checkbox("RSI")
+    if show_hyg == True:
+        show_hyg_label = st.text_input("RSI metric", "<20")
+    else:
+        pass
+
+with col4:
+    st.write("Market Thrust")
+    show_rsp = st.checkbox("RSP")
+
+with col5:
+    st.write("Yield Curve")
+    show_yield = st.checkbox("Yield Curve")
+    if show_yield == True:
+        show_hyg_label = st.radio('3-month vs ',
+                                  ["10-year", "30-year"])
+    else:
+        pass
