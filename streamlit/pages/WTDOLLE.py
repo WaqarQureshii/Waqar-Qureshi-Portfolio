7b4eb71a1db6c54a23667fd7828c28f0e807b206
SuperAngryFork <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-15 16:21:09 -0800

    Added Function importing

--- streamlit/pages/WTDOLLE.py
+++ streamlit/pages/WTDOLLE.py
@@ -1,21 +1,10 @@
-# import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-from functions.generate_db import *
-from functions.last_business_date import *
+import sys
+sys.path.append("..")
 
-interval_options = ['1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo']
+from functions.generate_db import generate_vix
 
-interval = '1wk'
-start_date = '2008-01-01'
-
-vix = generate_vix(start_date, interval = interval)
+vix = generate_vix('2008-01-01')
 print(vix)
-
-
-# today = today_date()
-
-# st.subheader("VIX")
-# print(today)
-
