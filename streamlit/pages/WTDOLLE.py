d02e76af259f465107123e1135322ddebb658919
SuperAngryFork <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-16 16:23:48 -0800

    Update WTDOLLE.py

--- streamlit/pages/WTDOLLE.py
+++ streamlit/pages/WTDOLLE.py
@@ -7,46 +7,67 @@ sys.path.append("..")
 
 from functions.generate_db import generate_vix
 
+st.set_page_config(layout="wider")
+st.write('''
+# WTDOLLE
+## This is what happened today with these variables:
+''')
 
-col1, col2, col3, col4, col5 = st.columns([0.2, 0.2, 0.2, 0.2, 0.2])
+with st.expander("Variables to include/exclude (select):"):
+    col1, col2, col3, col4, col5, col6, col7, col8= st.columns(8)
+    with col1:
+        st.write("Volatility Index")
+        
+        show_vix = st.checkbox('VIX', value=True)
+        if show_vix == True:
+            show_vix_label = st.radio('Choose Signal Type',
+                                    ['VIX % Change' ,"VIX level"],
+                                    index=0)
+        else:
+            pass
 
-with col1:
-    st.write("Volatility Index")
-    
-    show_vix = st.checkbox('VIX')
-    if show_vix == True:
-        show_vix_label = st.radio('Choose Signal Type',
-                                ["VIX level", 'VIX % Change'])
-    else:
-        pass
+    with col2:
+        st.write("Junk Bonds")
+        show_hyg = st.checkbox('HYG', value=True)
+        
+    with col3:
+        st.write("Overbought / Oversold")
+        show_rsi = st.checkbox("RSI", value=True)
+        if show_rsi == True:
+            show_rsi_label = st.text_input("RSI metric", "<20")
+        else:
+            pass
 
+    with col4:
+        st.write("Market Thrust")
+        show_rsp = st.checkbox("RSP")
 
-with col2:
-    st.write("Junk Bonds")
-    show_hyg = st.checkbox('HYG')
-    if show_hyg == True:
-        show_hyg_label = st.radio('Choose Signal Type',
-                                ["HYG level", 'HYG % Change'])
-    else:
-        pass
+    with col5:
+        st.write("Yield Curve")
+        show_yield = st.checkbox("Yield Curve", value=True)
+        if show_yield == True:
+            show_yield_label = st.radio('3-month vs ',
+                                    ["10-year", "30-year"],
+                                    index=1)
 
-with col3:
-    st.write("Overbought / Oversold")
-    show_RSI = st.checkbox("RSI")
-    if show_hyg == True:
-        show_hyg_label = st.text_input("RSI metric", "<20")
-    else:
-        pass
+            show_yield_metric = st.radio('Choose Yield Metric:',
+                                         ['Yield Curve % Change',
+                                          'Yield Curve Value'])
+        else:
+            pass
 
-with col4:
-    st.write("Market Thrust")
-    show_rsp = st.checkbox("RSP")
+    with col6:
+        st.write("Consumer")
+        show_consumer = st.checkbox("XLY")
 
-with col5:
-    st.write("Yield Curve")
-    show_yield = st.checkbox("Yield Curve")
-    if show_yield == True:
-        show_hyg_label = st.radio('3-month vs ',
-                                  ["10-year", "30-year"])
-    else:
-        pass
+    with col7:
+        st.write("Utility")
+        show_consumer = st.checkbox("XLU")
+
+    with col8:
+        st.write("Nasdaq vs SP500 Ratio")
+        show_ndxVSsp = st.checkbox("NDX vs SP500")
+        if show_ndxVSsp == True:
+            show_ndxVSsp_label = st.radio('NDX vs SP500',
+                                          ['% Change',
+                                           'Ratio'])
