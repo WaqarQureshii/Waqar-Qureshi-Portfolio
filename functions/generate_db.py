c827e33087f61ea251768a5b90737f295489c156
WaqarQureshii <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-19 19:24:36 -0800

    Moved generate_ta functions to generate_db

--- functions/generate_db.py
+++ functions/generate_db.py
@@ -1,4 +1,6 @@
 import yfinance as yf
+import pandas_ta as ta
+import pandas as pd
 
 def generate_sp500(start_date, end_date, interval = "1d"):
     sp500 = yf.download(['^GSPC'],
@@ -126,3 +128,55 @@ def generate_consumer(start_date, end_date, interval = '1d'):
     consumer['% Change'] = consumer['Close'].pct_change()
     
     return consumer
+
+def nasdaqvssp500(start_date, interval = '1d'):
+    nasdaq = generate_ndx(start_date, interval=interval)
+    nasdaq.drop(['Open', 'High', 'Low', 'Volume', '% Change'], axis = 1, inplace=True)
+    nasdaq.rename(columns={'Close':'Nasdaq Close'}, inplace=True)
+
+    sp500 = generate_sp500(start_date, interval=interval)
+    sp500.drop(['Open', 'High', 'Low', 'Volume', '% Change'], axis = 1, inplace=True)
+    sp500.rename(columns={'Close':'SP500 Close'}, inplace=True)
+
+    ndxvssp500 = pd.concat([nasdaq, sp500], axis=1)
+    ndxvssp500['Nasdaq vs SP500 Ratio'] = ndxvssp500['Nasdaq Close']/ndxvssp500['SP500 Close']
+    ndxvssp500.drop(['Nasdaq Close', 'SP500 Close'], axis = 1, inplace=True)
+    
+    return ndxvssp500
+
+
+def sp500_rsi(start_date,
+              end_date,
+              rsi_value = 22,
+              interval = '1d'):
+    sp500 = generate_sp500(start_date, end_date, interval = interval)
+    sp500[f'rsi'] = ta.rsi(close = sp500.Close, length=rsi_value)
+    return sp500
+
+
+def sp500_bbands(start_date,
+                 bbands_length = 20,
+                 bbands_std = 2):
+    sp500 = generate_sp500(start_date)
+    my_bbands = ta.bbands(close = sp500.Close, length=bbands_length, std=bbands_std)
+    # maybe look into renaming bbands if necessary
+    sp500 = sp500.join(my_bbands)
+    return sp500
+
+
+def yield_difference(start_date, lt_yield_inp = '30y'):
+    #LONG-TERM YIELD GENERATION
+    if lt_yield_inp == '30y':
+        lt_yield = generate_30yrx(start_date)
+    else:
+        lt_yield = generate_10yrx(start_date)
+    lt_yield.rename(columns={'Close': 'Long Term Yield'}, inplace=True)
+    #SHORT TERM YIELD GENERATION
+    
+    st_yield = generate_3mrx(start_date)
+    st_yield.rename(columns={'Close': 'Short Term Yield'}, inplace=True)
+
+    #CALCULATING YIELD DIFFERENCE
+    dt_yield_diff = pd.concat([lt_yield, st_yield], axis = 1)
+    dt_yield_diff['Yield Difference'] = dt_yield_diff['Long Term Yield'] - dt_yield_diff['Short Term Yield']
+    return dt_yield_diff
