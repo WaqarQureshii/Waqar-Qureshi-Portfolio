4bf07f6a62b62a8476b0bd7b3aed3c0b89ba32ee
WaqarQureshii <138326644+WaqarQureshii@users.noreply.github.com>
2023-11-28 22:45:12 -0800

    Filtered datasets based on intersection of inputs

--- streamlit/pages/1 - WTDOLLE.py
+++ streamlit/pages/1 - WTDOLLE.py
@@ -4,11 +4,11 @@ import pandas as pd
 import math
 from datetime import datetime
 import sys
+from functools import reduce
 sys.path.append("..")
 
 st.set_page_config(layout="wide")
 
-# from functions.generate_db import generate_vix, generate_hyg, generate_sp500
 from functions.generate_db import *
 from functions.last_business_date import *
 from datetime import datetime
@@ -70,6 +70,11 @@ if show_rsp == True:
     rsp_ma_length = st.sidebar.text_input("Input Moving Average Length (days)", 50)
     rsp_ma_length = int(rsp_ma_length)
     sidebar_counter += 1
+    rsp_comparator_selection = st.sidebar.radio('Choose RSP comparator',
+                                                ['Price greater than MA', "Price less than MA"],
+                                                index = 0)
+else:
+    pass
 st.sidebar.divider()
 
 #DISPLAY Yield Curve OPTIONS
@@ -126,6 +131,8 @@ st.markdown(
 column_list = st.columns(sidebar_counter)
 col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
 
+df_intersection = []
+
 #Displaying VIX Column
 with col1:
     if vix_show == True:
@@ -146,17 +153,19 @@ with col1:
 
             #Generate filtered dataset with selected parameters
             filtered_vix_metric = vix_metric[(vix_metric['% Change'] >= vix_pct_change_floor) & (vix_metric['% Change'] <= vix_pct_change_ceil)]
-
+            df_intersection.append(filtered_vix_metric)
         else:
             if vix_comparator_selection == 'Greater than:':
                 vix_boolean = vix_level > vix_comparator_value
-                st.metric(label=f'VIX @ {"{:.0f}".format(vix_level)}  > {vix_comparator_value}', value = vix_boolean)
+                st.metric(label=f'VIX > {vix_comparator_value}', value = f'{vix_boolean} @ {"{:.0f}".format(vix_level)}')
                 filtered_vix_metric = vix_metric[vix_metric['Close'] > vix_comparator_value]
+                df_intersection.append(filtered_vix_metric)
 
             else:
                 vix_boolean = vix_level < vix_comparator_value
-                st.metric(label = f'VIX @ {"{:.0f}".format(vix_level)} < {vix_comparator_value}', value = vix_boolean)
+                st.metric(label=f'VIX < {vix_comparator_value}', value = f'{vix_boolean} @ {"{:.0f}".format(vix_level)}')
                 filtered_vix_metric = vix_metric[vix_metric['Close'] < vix_comparator_value]
+                df_intersection.append(filtered_vix_metric)
 
 with col2:
      if hyg_show == True:
@@ -171,7 +180,8 @@ with col2:
 
         #Generate filtered dataset with selected parameters
         filtered_hyg_metric = hyg_metric[(hyg_metric['% Change'] >= hyg_pct_change_floor) & (hyg_metric['% Change'] <= hyg_pct_change_ceil)]
-        
+        df_intersection.append(filtered_hyg_metric)
+
         #Show metric
         st.metric(label = "HYG % Change", value = str_hyg_pct_change)
 
@@ -185,17 +195,19 @@ with col3:
 
         if rsi_comparator_selection == "Greater than:":
             rsi_boolean = rsi_level > rsi_comparator_value
-            st.metric(label=f'RSI @ {"{:.0f}".format(rsi_level)} > {rsi_comparator_value}', value = rsi_boolean)
+            st.metric(label=f'RSI > {rsi_comparator_value}', value = f'{rsi_boolean} @ {"{:.0f}".format(rsi_level)}')
 
             #Generate filtered dataset with selected parameters
             filtered_rsi_metric = rsi_metric[rsi_metric['rsi'] > rsi_comparator_value]
+            df_intersection.append(filtered_rsi_metric)
 
         else:
             rsi_boolean = rsi_level < rsi_comparator_value
-            st.metric(label=f'RSI @ {"{:.0f}".format(rsi_level)} < {rsi_comparator_value}', value = rsi_boolean)
+            st.metric(label=f'RSI < {rsi_comparator_value}', value = f'{rsi_boolean} @ {"{:.0f}".format(rsi_level)}')
 
             #Generate filtered dataset with selected parameters
             filtered_rsi_metric = rsi_metric[rsi_metric['rsi'] < rsi_comparator_value]
+            df_intersection.append(filtered_rsi_metric)
 
 with col4:
     if show_rsp == True:
@@ -206,8 +218,34 @@ with col4:
         #Grab inputted date's RSP Price and Moving Average
         rsp_price = rsp_metric["Close"].iloc[-1]
         rsp_ma = rsp_metric['ma'].iloc[-1]
-        rsp_boolean = rsp_price > rsp_ma
-        st.metric(label=f'RSP @ {rsp_price}; MA: {rsp_ma}', value = rsp_boolean)
 
+        if rsp_comparator_selection == 'Price greater than MA':
+            rsp_boolean = rsp_price > rsp_ma
+            st.metric(label=f'Price > MA {"{:.0f}".format(rsp_ma)}', value = f'{rsp_boolean} @ {"{:.0f}".format(rsp_price)}')
+            filtered_rsp_metric = rsp_metric[rsp_metric['Close'] > rsp_metric['ma']]
+            df_intersection.append(filtered_rsp_metric)
 
-# st.dataframe(data = filtered_rsi_metric)
+        else:
+            rsp_boolean = rsp_price < rsp_ma
+            st.metric(label=f'Price < MA {"{:.0f}".format(rsp_ma)}', value = f'{rsp_boolean} @ {"{:.0f}".format(rsp_price)}')
+            filtered_rsp_metric = rsp_metric[rsp_metric['Close'] < rsp_metric['ma']]
+            df_intersection.append(filtered_rsp_metric)
+
+index_columns = [df.index for df in df_intersection]
+common_index = reduce(lambda left, right: left.intersection(right), index_columns)
+st.write(common_index)
+
+# unindexed_filtered_hyg_metric = filtered_hyg_metric.reset_index()
+# unindexed_filtered_vix_metric = filtered_vix_metric.reset_index()
+# unindexed_filtered_rsp_metric = filtered_rsp_metric.reset_index()
+# dates1 = set(unindexed_filtered_hyg_metric['Date'])
+# dates2 = set(unindexed_filtered_rsp_metric['Date'])
+# dates3 = set(unindexed_filtered_vix_metric['Date'])
+
+# intersection = dates3, dates2
+# common_dates = dates1.intersection(intersection)
+
+# st.write(common_dates)
+# printer = filtered_hyg_metric.columns
+# st.write(printer)
+# st.dataframe(data = unindexed_filtered_hyg_metric)
