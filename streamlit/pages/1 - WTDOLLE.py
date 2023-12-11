9a2862019f50068264ba6ae29569df5ba684d56e
WaqarQureshii <138326644+WaqarQureshii@users.noreply.github.com>
2023-12-10 22:15:46 -0800

    Individual Indices parameter options set up

--- streamlit/pages/1 - WTDOLLE.py
+++ streamlit/pages/1 - WTDOLLE.py
@@ -150,28 +150,32 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+# --- DATAFRAMES SET UP ---
+# ---------- DATAFRAMES FOR COMMON DATE INDICES --------------
+df_intersection = [] # TODO remove df_intersection once indices are set up.
+sp500_intersection = []
+nasdaq_intersection = []
+rus2k_intersection = []
+# --------------- DATAFRAMES FOR ALL INDICES ------------------
+vix_metric = generate_vix(start_date, input_end_date, interval_input)
+hyg_metric = generate_hyg(start_date, input_end_date, interval_input)
+rsi_metric = sp500_rsi(start_date, input_end_date, int(rsi_length), interval_input)
+rsp_metric = generate_rsp(start_date, input_end_date, interval_input, ma_length = rsp_ma_length)
+sp500 = generate_sp500(start_date, input_end_date)
+ndx = generate_ndx(start_date, input_end_date)
+
 
 # ----- SELECTED VARIABLES COLUMNS ---------
 col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
 
-# --- DATAFRAMES SET UP ---
-df_intersection = []
-sp500_intersection = []
-nasdaq_intersection = []
-rus2k_intersection = []
-vix_metric = generate_vix(start_date, input_end_date, interval_input)
-
-
-#Displaying VIX Column
+# ------------------ ESTABLISH VIX CLOSING PRICE AND LAST % CHANGE FOR WTDOLLE ----------------------
 with col1:
     if vix_show == True:
         
-        #grab the closing price of the inputted end date
         vix_level = vix_metric["Close"].iloc[-1]
-
-        #grab the inputted end date's % change
         str_vix_pct_change = "{:.2%}".format(vix_metric["% Change"].iloc[-1])
         int_vix_pct_change = vix_metric["% Change"].iloc[-1]
+
 
         if vix_show_label == "VIX % Change":
             st.metric(label = "VIX % Change", value = str_vix_pct_change)
@@ -181,23 +185,30 @@ with col1:
             #Generate filtered dataset with selected parameters
             filtered_vix_metric = vix_metric[(vix_metric['% Change'] >= vix_pct_change_floor) & (vix_metric['% Change'] <= vix_pct_change_ceil)]
             df_intersection.append(filtered_vix_metric)
+            sp500_intersection.append(filtered_vix_metric)
+            nasdaq_intersection.append(filtered_vix_metric)
+            rus2k_intersection.append(filtered_vix_metric)
         else:
             if vix_comparator_selection == 'Greater than:':
                 vix_boolean = vix_level > vix_comparator_value
                 st.metric(label=f'VIX > {vix_comparator_value}', value = f'{vix_boolean} @ {"{:.0f}".format(vix_level)}')
                 filtered_vix_metric = vix_metric[vix_metric['Close'] > vix_comparator_value]
                 df_intersection.append(filtered_vix_metric)
+                sp500_intersection.append(filtered_vix_metric)
+                nasdaq_intersection.append(filtered_vix_metric)
+                rus2k_intersection.append(filtered_vix_metric)
 
             else:
                 vix_boolean = vix_level < vix_comparator_value
                 st.metric(label=f'VIX < {vix_comparator_value}', value = f'{vix_boolean} @ {"{:.0f}".format(vix_level)}')
                 filtered_vix_metric = vix_metric[vix_metric['Close'] < vix_comparator_value]
                 df_intersection.append(filtered_vix_metric)
+                sp500_intersection.append(filtered_vix_metric)
+                nasdaq_intersection.append(filtered_vix_metric)
+                rus2k_intersection.append(filtered_vix_metric)
 
 with col2:
      if hyg_show == True:
-        #Generate entire VIX db with start, inputted end date and interval
-        hyg_metric = generate_hyg(start_date, input_end_date, interval_input)
 
         #Grab the inputted end date's % Change
         str_hyg_pct_change = "{:.2%}".format(hyg_metric["% Change"].iloc[-1])
@@ -208,14 +219,15 @@ with col2:
         #Generate filtered dataset with selected parameters
         filtered_hyg_metric = hyg_metric[(hyg_metric['% Change'] >= hyg_pct_change_floor) & (hyg_metric['% Change'] <= hyg_pct_change_ceil)]
         df_intersection.append(filtered_hyg_metric)
+        sp500_intersection.append(filtered_hyg_metric)
+        nasdaq_intersection.append(filtered_hyg_metric)
+        rus2k_intersection.append(filtered_hyg_metric)
 
         #Show metric
         st.metric(label = "HYG % Change", value = str_hyg_pct_change)
 
 with col3:
     if rsi_show == True:
-        #Generate RSI metric
-        rsi_metric = sp500_rsi(start_date, input_end_date, int(rsi_length), interval_input)
         
         #Grab the inputted end date's metric
         rsi_level = rsi_metric["rsi"].iloc[-1]
@@ -227,6 +239,9 @@ with col3:
             #Generate filtered dataset with selected parameters
             filtered_rsi_metric = rsi_metric[rsi_metric['rsi'] > rsi_comparator_value]
             df_intersection.append(filtered_rsi_metric)
+            sp500_intersection.append(filtered_hyg_metric)
+            nasdaq_intersection.append(filtered_hyg_metric)
+            rus2k_intersection.append(filtered_hyg_metric)
 
         else:
             rsi_boolean = rsi_level < rsi_comparator_value
@@ -235,12 +250,12 @@ with col3:
             #Generate filtered dataset with selected parameters
             filtered_rsi_metric = rsi_metric[rsi_metric['rsi'] < rsi_comparator_value]
             df_intersection.append(filtered_rsi_metric)
+            sp500_intersection.append(filtered_hyg_metric)
+            nasdaq_intersection.append(filtered_hyg_metric)
+            rus2k_intersection.append(filtered_hyg_metric)
 
 with col4:
     if show_rsp == True:
-        
-        #Generate database for selected input
-        rsp_metric = generate_rsp(start_date, input_end_date, interval_input, ma_length = rsp_ma_length)
 
         #Grab inputted date's RSP Price and Moving Average
         rsp_price = rsp_metric["Close"].iloc[-1]
@@ -251,120 +266,140 @@ with col4:
             st.metric(label=f'Price > MA {"{:.0f}".format(rsp_ma)}', value = f'{rsp_boolean} @ {"{:.0f}".format(rsp_price)}')
             filtered_rsp_metric = rsp_metric[rsp_metric['Close'] > rsp_metric['ma']]
             df_intersection.append(filtered_rsp_metric)
+            sp500_intersection.append(filtered_rsp_metric)
+            nasdaq_intersection.append(filtered_rsp_metric)
+            rus2k_intersection.append(filtered_rsp_metric)
 
         else:
             rsp_boolean = rsp_price < rsp_ma
             st.metric(label=f'Price < MA {"{:.0f}".format(rsp_ma)}', value = f'{rsp_boolean} @ {"{:.0f}".format(rsp_price)}')
             filtered_rsp_metric = rsp_metric[rsp_metric['Close'] < rsp_metric['ma']]
             df_intersection.append(filtered_rsp_metric)
+            sp500_intersection.append(filtered_rsp_metric)
+            nasdaq_intersection.append(filtered_rsp_metric)
+            rus2k_intersection.append(filtered_rsp_metric)
 
 st.write("")
 st.write("")
 
-fig1, fig2, fig3 = st.columns(3)
-with fig1:
-    fig1col1, fig1col2, fig1col3 = st.columns(3)
-    with fig1col1:
-        sp500_interval_selection = int(st.text_input("Input # of intervals to calculate s&p 500 return:", 10))
-    with fig1col2:
-        sp500rsishow = st.checkbox("Overbought/Oversold RSI Indicator for S&P500", value=False)
-        pass
-    with fig1col3:
-        if sp500rsishow == True:
-            sp500_rsi_length = st.text_input("Select the S&P RSI length (in days)", 10)
+# --------- PARAMETER SELECTIONS FOR INDIVIDUAL INDICES ---------
+parameter_expander = st.expander("Choose additional parameters specific to each entity")
+with parameter_expander:
+    fig1, fig2, fig3 = st.columns(3)
 
-    index_columns = [df.index for df in df_intersection]
-    common_index = reduce(lambda left, right: left.intersection(right), index_columns).to_list()
+# -------------------- S&P500 PARAMETER SELECTION ---------------------
+    with fig1:
+        st.subheader("S&P 500")
+        fig1col1, fig1col2 = st.columns(2)
+        with fig1col1:
+            sp500_interval_selection = int(st.text_input("Calculate return over a selected # of intervals:", 10, key="sp500 interval selection"))
+        with fig1col2:
+            sp500rsishow = st.checkbox("Overbought/Oversold RSI Indicator", value=False, key="sp500 rsi indicator selection")
+            if sp500rsishow == True:
+                sp500comparator = st.radio('Choose comparator',
+                                                      ['Greater than:', "Less than:"],
+                                                      index = 1,
+                                                      key = 'sp500 comparator selection')
+                sp500_rsi_length = st.text_input("Select the RSI length (in intervals)", 10, key = "sp500 rsi length selector")
 
-    # generates panda datafrane
-    sp500 = generate_sp500(start_date, input_end_date)
-    sp500['% Change Sel Interval'] = sp500['Close'].pct_change(sp500_interval_selection).shift(-sp500_interval_selection)
+# -------------------------------- S&P500 DATAFRAME GENERATION -------------------------------
+        sp500_index_columns = [df.index for df in df_intersection]
+        sp500_common_index = reduce(lambda left, right: left.intersection(right), sp500_index_columns).to_list()
 
-    sp500_common = sp500[sp500.index.isin(common_index)]
+        # generates panda datafrane
+        sp500['% Change Sel Interval'] = sp500['Close'].pct_change(sp500_interval_selection).shift(-sp500_interval_selection)
 
-    fig, ax = plt.subplots()
-    ax.set_title('S&P500')
-    ax.plot(sp500.index, sp500['Close'], linewidth = 0.5, color='black')
-    ax.scatter(sp500_common.index, sp500_common['Close'], marker='.', color='red', s = 10)
-    st.pyplot(fig)
+        sp500_common = sp500[sp500.index.isin(sp500_common_index)]
 
-    average_sp500_return = sp500_common['% Change Sel Interval'].mean()
-    sp500_number_of_occurrences = len(sp500_common['% Change Sel Interval'])
-    sp500_number_of_positives = (sp500_common['% Change Sel Interval'] > 0).sum()
-    sp500_positive_percentage = sp500_number_of_positives/sp500_number_of_occurrences
-    sp500_positive_percentage = '{:.2%}'.format(sp500_positive_percentage)
+        fig, ax = plt.subplots()
+        ax.set_title('S&P500')
+        ax.plot(sp500.index, sp500['Close'], linewidth = 0.5, color='black')
+        ax.scatter(sp500_common.index, sp500_common['Close'], marker='.', color='red', s = 10)
+        st.pyplot(fig)
+#--------------------------------------S&P500 GENERATE VALUES FOR STRING STATEMENTS-------------------------------
+        average_sp500_return = sp500_common['% Change Sel Interval'].mean()
+        sp500_number_of_occurrences = len(sp500_common['% Change Sel Interval'])
+        sp500_number_of_positives = (sp500_common['% Change Sel Interval'] > 0).sum()
+        sp500_positive_percentage = sp500_number_of_positives/sp500_number_of_occurrences
+        sp500_positive_percentage = '{:.2%}'.format(sp500_positive_percentage)
 
-    st.write(f'This occurred {sp500_number_of_occurrences} of time(s) and is {sp500_positive_percentage} positive in {sp500_interval_selection} days.' )
-    st.write('{:.2%}'.format(average_sp500_return))
-    # st.table(data=sp500)
+        st.write(f'This occurred {sp500_number_of_occurrences} of time(s) and is {sp500_positive_percentage} positive in {sp500_interval_selection} days.' )
+        st.write('{:.2%}'.format(average_sp500_return))
 
+# -------------------- NASDAQ PARAMETER SELECTION ---------------------
+    with fig2:
+        st.subheader("Nasdaq")
+        fig2col1, fig2col2 = st.columns(2)
+        with fig2col1:
+            nasdaq_interval_selection = int(st.text_input("Calculate return over a selected # of intervals:", 10, key="nasdaq interval selection"))
+        with fig2col2:
+            ndxrsishow = st.checkbox("Overbought/Oversold RSI Indicator", value=False, key="nasdaq rsi indicator selection")
+            if ndxrsishow == True:
+                nasdaqcomparator = st.radio('Choose comparator',
+                                                      ['Greater than:', "Less than:"],
+                                                      index = 1,
+                                                      key = 'nasdaq comparator selection')
+                nasdaq_rsi_length = st.text_input("Select the RSI length (in intervals)", 10, key = "nasdaq rsi length selector")
+        
+        # -------------------------------- NASDAQ DATAFRAME GENERATION -------------------------------
+        index_columns = [df.index for df in df_intersection]
+        common_index = reduce(lambda left, right: left.intersection(right), index_columns).to_list()
 
-with fig2:
-    fig2col1, fig2col2, fig2col3 = st.columns(3)
-    with fig2col1:
-        nasdaq_interval_selection = int(st.text_input("Input # of intervals to calculate Nasdaq return:", 10))
-    with fig2col2:
-        ndxrsishow = st.checkbox("Overbought/Oversold RSI Indicator for Nasdaq", value=False)
-        pass
-    with fig2col3:
-        if ndxrsishow == True:
-            ndx_rsi_length = st.text_input("Select the Nasdaq RSI length (in days)", 10)
-    
-    index_columns = [df.index for df in df_intersection]
-    common_index = reduce(lambda left, right: left.intersection(right), index_columns).to_list()
+        ndx['% Change Sel Interval'] = ndx['Close'].pct_change(nasdaq_interval_selection).shift(-nasdaq_interval_selection)
 
-    # generates panda datafrane
-    ndx = generate_ndx(start_date, input_end_date)
-    ndx['% Change Sel Interval'] = ndx['Close'].pct_change(nasdaq_interval_selection).shift(-nasdaq_interval_selection)
+        ndx_common = ndx[ndx.index.isin(common_index)]
 
-    ndx_common = ndx[ndx.index.isin(common_index)]
+        fig, ax = plt.subplots()
+        ax.set_title('Nasdaq 100')
+        ax.plot(ndx.index, ndx['Close'], linewidth = 0.5, color='black')
+        ax.scatter(ndx_common.index, ndx_common['Close'], marker='.', color='red', s = 10)
+        st.pyplot(fig)
+#--------------------------------------NASDAQ GENERATE VALUES FOR STRING STATEMENTS-------------------------------
+        average_ndx_return = ndx_common['% Change Sel Interval'].mean()
+        ndx_number_of_occurrences = len(ndx_common['% Change Sel Interval'])
+        ndx_number_of_positives = (ndx_common['% Change Sel Interval'] > 0).sum()
+        ndx_positive_percentage = ndx_number_of_positives/ndx_number_of_occurrences
+        ndx_positive_percentage = '{:.2%}'.format(ndx_positive_percentage)
 
-    fig, ax = plt.subplots()
-    ax.set_title('Nasdaq 100')
-    ax.plot(ndx.index, ndx['Close'], linewidth = 0.5, color='black')
-    ax.scatter(ndx_common.index, ndx_common['Close'], marker='.', color='red', s = 10)
-    st.pyplot(fig)
+        st.write(f'This occurred {ndx_number_of_occurrences} time(s) and is {ndx_positive_percentage} positive in {nasdaq_interval_selection} days.' )
+        st.write('{:.2%}'.format(average_ndx_return))
 
-    average_ndx_return = ndx_common['% Change Sel Interval'].mean()
-    ndx_number_of_occurrences = len(ndx_common['% Change Sel Interval'])
-    ndx_number_of_positives = (ndx_common['% Change Sel Interval'] > 0).sum()
-    ndx_positive_percentage = ndx_number_of_positives/ndx_number_of_occurrences
-    ndx_positive_percentage = '{:.2%}'.format(ndx_positive_percentage)
+# -------------------- RUS2K PARAMETER SELECTION ---------------------
+    with fig3:
+        st.subheader("Russel 2000")
+        fig3col1, fig3col2 = st.columns(2)
+        with fig3col1:
+            rus2k_interval_selection = int(st.text_input("Calculate return over a selected # of intervals:", 10, key="rus2k interval selection"))
+        with fig3col2:
+            rus2krsishow = st.checkbox("Overbought/Oversold RSI Indicator", value=False, key="rus2k rsi indicator selection")
+            if rus2krsishow == True:
+                rus2kcomparator = st.radio('Choose comparator',
+                                                      ['Greater than:', "Less than:"],
+                                                      index = 1,
+                                                      key = 'rus2k comparator selection')
+                rus2k_rsi_length = st.text_input("Select the RSI length (in intervals)", 10, key = "rus2k rsi length selector")
+        
+# -------------------------------- RUSSEL 2000 DATAFRAME GENERATION -------------------------------
+        index_columns = [df.index for df in df_intersection]
+        common_index = reduce(lambda left, right: left.intersection(right), index_columns).to_list()
 
-    st.write(f'This occurred {ndx_number_of_occurrences} time(s) and is {ndx_positive_percentage} positive in {nasdaq_interval_selection} days.' )
-    st.write('{:.2%}'.format(average_ndx_return))
+        # generates panda datafrane
+        rus2k = generate_rus2k(start_date, input_end_date)
+        rus2k['% Change Sel Interval'] = rus2k['Close'].pct_change(rus2k_interval_selection).shift(-rus2k_interval_selection)
 
-with fig3:
-    fig3col1, fig3col2, fig3col3 = st.columns(3)
-    with fig3col1:
-        rus2k_interval_selection = int(st.text_input("Input # of intervals to calculate Russel 2000 return:", 10))
-    with fig3col2:
-        rus2krsishow = st.checkbox("Overbought/Oversold RSI Indicator for Russel 2000", value=False)
-        pass
-    with fig3col3:
-        if rus2krsishow == True:
-            rus2k_rsi_length = st.text_input("Select the Russel 2000 RSI length (in days)", 10)
-    
-    index_columns = [df.index for df in df_intersection]
-    common_index = reduce(lambda left, right: left.intersection(right), index_columns).to_list()
+        rus2k_common = rus2k[rus2k.index.isin(common_index)]
 
-    # generates panda datafrane
-    rus2k = generate_rus2k(start_date, input_end_date)
-    rus2k['% Change Sel Interval'] = rus2k['Close'].pct_change(rus2k_interval_selection).shift(-rus2k_interval_selection)
+        fig, ax = plt.subplots()
+        ax.set_title('Russel 2000')
+        ax.plot(rus2k.index, rus2k['Close'], linewidth = 0.5, color='black')
+        ax.scatter(rus2k_common.index, rus2k_common['Close'], marker='.', color='red', s = 10)
+        st.pyplot(fig)
+#--------------------------------------RUSSEL 2000 GENERATE VALUES FOR STRING STATEMENTS-------------------------------
+        average_rus2k_return = rus2k_common['% Change Sel Interval'].mean()
+        rus2k_number_of_occurrences = len(rus2k_common['% Change Sel Interval'])
+        rus2k_number_of_positives = (rus2k_common['% Change Sel Interval'] > 0).sum()
+        rus2k_positive_percentage = rus2k_number_of_positives/rus2k_number_of_occurrences
+        rus2k_positive_percentage = '{:.2%}'.format(rus2k_positive_percentage)
 
-    rus2k_common = rus2k[rus2k.index.isin(common_index)]
-
-    fig, ax = plt.subplots()
-    ax.set_title('Russel 2000')
-    ax.plot(rus2k.index, rus2k['Close'], linewidth = 0.5, color='black')
-    ax.scatter(rus2k_common.index, rus2k_common['Close'], marker='.', color='red', s = 10)
-    st.pyplot(fig)
-
-    average_rus2k_return = rus2k_common['% Change Sel Interval'].mean()
-    rus2k_number_of_occurrences = len(rus2k_common['% Change Sel Interval'])
-    rus2k_number_of_positives = (rus2k_common['% Change Sel Interval'] > 0).sum()
-    rus2k_positive_percentage = rus2k_number_of_positives/rus2k_number_of_occurrences
-    rus2k_positive_percentage = '{:.2%}'.format(rus2k_positive_percentage)
-
-    st.write(f'This occurred {rus2k_number_of_occurrences} of time(s) and is {rus2k_positive_percentage} positive in {rus2k_interval_selection} days.' )
-    st.write('{:.2%}'.format(average_rus2k_return))
+        st.write(f'This occurred {rus2k_number_of_occurrences} of time(s) and is {rus2k_positive_percentage} positive in {rus2k_interval_selection} days.' )
+        st.write('{:.2%}'.format(average_rus2k_return))
