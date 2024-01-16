f8c495ada0535c78109854925759cc372d774c7a
WaqarQureshii <138326644+WaqarQureshii@users.noreply.github.com>
2024-01-15 18:54:08 -0800

    Updated Homepage with description and "About"

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -4,6 +4,9 @@ st.title("What Transpired During Our Last Encounter? WTDOLLE")
 
 st.subheader("About")
 st.write('''
-        This is line 1\n
-        This is line 2 
+        Have you ever been looking to invest in the US stock market such as the Nasdaq, S&P500 and the Russel 2000? There are many variables to consider and a lot of correlations (sometimes causations) in the market. Some signals are leading indicators of a bull run, some are after-the-fact indicators of a bull run. \n
+        
+         The purpose of WTDOLLE (What Transpired During Our Last Encounter) is to answer this very question: what was the general return over a given number of periods when the last time certain signals were given. \n
+
+         Head over to the WTDOLLE page to see all of the available pages.
 ''')
