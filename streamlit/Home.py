139b65a42557427d7daa9cd4fbc564ad26642e16
WaqarQureshii <138326644+WaqarQureshii@users.noreply.github.com>
2024-08-20 15:18:45 -0700

    Update Home.py

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -18,22 +18,17 @@ st.write_stream(streamhowitbegan)
 # \\\\\\\\\\ THE JOURNEY //////////
 st.header(":blue[Project/s (adding these in every day)]")
 st.subheader("WTDOLLE - What Transpired During Our Last Encounter")
-def streamdatatoinsights():
-        fromdatatoinsights ='''This tool is used to figure out when in the S&P500, Nasdaq or Russell 2000 did X go up X% and Y was below price 40 (example).
-
-        It utilizes the following:
-        Python programming
+st.markdown('''
+This tool is used to figure out when in the S&P500, Nasdaq or Russell 2000 did X go up X% and Y was below price 40 (example). It utilizes the following:''')
+st.markdown(':snake: Programming in Python: libraries such as Pandas (migrating to Polars) for large data extraction, transformation and loading (ETL), Streamlit (what this app is built on), matplotlib for graphing.')
+st.markdown(':bar_chart: Skillsets working with large data from working with Power BI and other visualization tools')
+st.markdown(':heavy_dollar_sign: Statistical and financial background knowledge from my working knowledge in the industry')
+st.markdown(":computer: Utilizing and following API documentation such as Yahoo Finance, Streamlit and Federal Reserve Economic Data (FRED).")
+      
+# Statistical and financial background knowledge from my working knowledge in the industry
         
-        Skillsets working with large data from working with Power BI and other visualization tools,
+#         Specific Python libraries and APIs: Pandas, numpy, Polars (moving towards Polars instead of Pandas), streamlit (what this app is hosted on), Yahoo 
+#         Finance's API, FRED (Federal Reserve Economic Data)'s API.
         
-        Statistical and financial background knowledge from my working knowledge in the industry
-        
-        Specific Python libraries and APIs: Pandas, numpy, Polars (moving towards Polars instead of Pandas), streamlit (what this app is hosted on), Yahoo 
-        Finance's API, FRED (Federal Reserve Economic Data)'s API.
-        
-I call it What Transpired During Our Last Encounter [WTDOLLE](WTDOLLE).'''
-        for word in fromdatatoinsights.split(" "):
-                yield word + " "
-                time.sleep(0.01)
-st.write_stream(streamdatatoinsights)
+# I call it What Transpired During Our Last Encounter [WTDOLLE](WTDOLLE).
 
