1ec5b74eaea3241621df031fb8d0014a74065296
WaqarQureshii <138326644+WaqarQureshii@users.noreply.github.com>
2024-09-16 11:44:28 -0700

    Reverted back to Pandas for Debt
    Had to move debt markets database filtering to pandas to play well with existing filtering with equity markets.

--- streamlit/functions/generation_debt.py
+++ streamlit/functions/generation_debt.py
@@ -38,7 +38,6 @@ class Generate_Yield():
 
         Output:
         '''
-        # fred_key = st.secrets[fred_API_key]
         frequency_options = {"Daily": "d", "Weekly": "w", "Monthly": "m"}
         fred_api = st.secrets.fred_api
         fred = Fred(api_key=fred_api)
@@ -136,11 +135,23 @@ class Generate_Yield():
         return sp500, ndx, rus2k
 
 
-class Generate_Yields():
+class Generate_Yield_panda():
     def __init__(self):
         self.db = None
         self.db_graph = None
-
+        self.maturity_options = {
+            "1m": "DGS1MO",
+            "3m": "DGS3MO",
+            "6m": "DGS6MO",
+            "1y": "DGS1",
+            "2y": "DGS2",
+            "3y": "DGS3",
+            "5y": "DGS5",
+            "7y": "DGS7",
+            "10y": "DGS10",
+            "20y": "DGS20",
+            "30y": "DGS30",
+        }
         self.yield_ratio = None
         self.curr_yieldratio = None
         self.curr_ltyield = None
@@ -149,62 +160,111 @@ class Generate_Yields():
         self.curr_yielddiff = None
         self.curr_yieldratio = None
 
-    def generate_db(self, term_yield: str, start_date: str, end_date: str, interval: str = "1d"):
-        """
-        Provide the ticker for the term yield and generates a database; can either be:
-        - 3 month: ^IRX
-        - 10 year: ^TNX
-        - 30 year: ^TYX
-        """
-        self.db = yf.download([term_yield], start_date, end_date, interval=interval)
-        self.db_graph = self.db.drop(["Volume", 'Open', 'High', 'Low', 'Adj Close'], axis=1)
-        self.db_graph['% Change'] = self.db['Close'].pct_change()
+    def get_database(self, selection: str, start_date, end_date, frequency):
+        '''
+        Provide bond yield maturity length, start and end dates, and frequency 
+        
+        Args:
+        selection: str -> 1m, 3m, 6m, 1y, 2y, 3y, 5y, 7y, 10y, 20y, 30y
+        frequency: str -> Daily, Weekly, Monthly
+        start_date: str -> 2023-01-01
+        end_date: str -> 2023-12-31
 
-        return self.db_graph
+        Output:'''
+        frequency_options = {"Daily": "d", "Weekly": "w", "Monthly": "m"}
+        fred_api = st.secrets.fred_api
+        fred = Fred(api_key=fred_api)
+        return fred.get_series(self.maturity_options.get(selection), start_date, end_date, frequency=frequency_options.get(frequency))
     
-    def generate_yield_ratio(self, start_date, end_date, interval, numerator: str, denominator: str):
-        """
-        Provide a numerator that is either 3 month (^IRX) or 10 year (^TNX) and a denominator that is either 10 year (^TNX) or 30 year (^TYX), returns a ratio between the numerator and denominator in a database.
-        """
-        # Generating Short and Long-Term databases
-        self.num_yield, self.den_yield = self.generate_db(term_yield=numerator, start_date=start_date, end_date=end_date, interval=interval), self.generate_db(term_yield=denominator, start_date=start_date, end_date=end_date, interval=interval)
-        self.num_yield.rename(columns={'Close': "Short Term Yield"}, inplace=True)
-        self.den_yield.rename(columns={'Close': "Long Term Yield"}, inplace=True)
+    def generate_yield_spread(self, start_date, end_date, frequency, long_term_yield:str, short_term_yield:str) -> None:
+        '''
+        Provide two selections of 2 different maturity terms, to return a LazyFrame with a spread between the two.
+        Typically the longer yield term is in the numerator.\n
 
-        # Calculates Yield Figures
-        self.yield_ratio = pd.concat([self.num_yield, self.den_yield], axis=1)
-        self.yield_ratio['Yield Ratio'] = self.yield_ratio['Short Term Yield']/self.yield_ratio['Long Term Yield']
-        self.yield_ratio['Yield Ratio % Chg'] = self.yield_ratio['Yield Ratio'].pct_change()
+        Args:
+        long_term_yield: str -> 1m, 3m, 6m, 1y, 2y, 3y, 5y, 7y, 10y, 20y, 30y
+        short_term_yield: str -> 1m, 3m, 6m, 1y, 2y, 3y, 5y, 7y, 10y, 20y, 30y
+        frequency: str -> Daily, Weekly, Monthly
+        start_date: str -> 2023-01-01
+        end_date: str -> 2023-12-31
+        '''
 
-        # Value for UI
-        self.curr_yieldratio = round(self.yield_ratio['Yield Ratio'].iloc[-1],2)
-        self.curr_ltyield = round(self.yield_ratio['Long Term Yield'].iloc[-1],2)
-        self.curr_styield = round(self.yield_ratio['Short Term Yield'].iloc[-1],2)
+        self.ltyield = self.get_database(long_term_yield, start_date, end_date, frequency)
+        self.ltyield=pd.DataFrame(self.ltyield, columns=[f'{long_term_yield} Rate'])
+        self.ltyield.index = pd.to_datetime(self.ltyield.index, format='%Y/%m/%d')
 
-    def metric_vs_selection(self, comparison_type:str, comparator:str, selected_value, sp500:pd.DataFrame, ndx:pd.DataFrame, rus2k:pd.DataFrame):
-        type_dicts = {
-            "ratio vs selection": {
-                "1st value": self.curr_yieldratio,
-                "2nd value": selected_value,
-                "1st col": self.yield_ratio['Yield Ratio'],
-                "2nd col": selected_value
+        self.styield = self.get_database(short_term_yield, start_date, end_date, frequency)
+        self.styield = pd.DataFrame(self.styield, columns=[f'{short_term_yield} Rate'])
+        self.styield.index = pd.to_datetime(self.styield.index, format='%Y/%m/%d')
+
+        self.yielddiff_df = pd.concat([self.ltyield, self.styield], axis=1)
+        
+        # Calculate yield spread (lt - st)
+        self.yielddiff_df['Rate Spread'] = self.yielddiff_df[f'{long_term_yield} Rate'] - self.yielddiff_df[f'{short_term_yield} Rate']
+        self.yielddiff_df['% Change'] = self.yielddiff_df['Rate Spread'].pct_change()*100
+
+
+    def metric_vs_selection_cross(self, comparison_type: str, selected_value: list, sp500: pd.DataFrame, ndx:pd.DataFrame, rus2k:pd.DataFrame, comparator:str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+        '''
+        Provide comparison metrics and return back dates in the sp500, ndx, and rus2k DataFrames where these comparison logics were applied.
+
+        Args:\n
+        comparison_type: str -> current price, % change between\n
+        selected_value: list -> range between two numbers, where the first number is the lower value\n
+        comparator: str -> Greater than, Lower than, Between\n
+
+        Output:
+        self, sp500_intersection, ndx_intersection, rus2k_intersection
+        '''
+        
+        if len(selected_value)==2:
+            comparison_value_lower = selected_value[0]
+            comparison_value_higher = float(selected_value[1])
+        elif len(selected_value)==1:
+            comparison_value_lower = selected_value[0]
+            comparison_value_higher = None
+        elif len(selected_value)==0:
+            comparison_value_lower = None
+            comparison_value_higher = None
+        else:
+            comparison_value_higher=None
+
+        selection_dict = {
+            "current price": {
+                "current db value": self.yielddiff_df['Rate Spread'][-1],
+                "db values": "Rate Spread",
+                "db values x": 1,
+                "comparison value lower": comparison_value_lower,
+                "comparison value higher": 0
+            },
+            "% change between": {
+                "current db value": self.yielddiff_df['% Change'][-1],
+                "db values": "% Change",
+                "db values x": 100,
+                "comparison value lower": comparison_value_lower,
+                "comparison value higher": comparison_value_higher
             }
         }
+        print(f'Selected value: {selected_value[0]}')
+        print(f'')
         if comparator == 'Greater than':
-            self.boolean_comp = type_dicts[comparison_type]['1st value'] > type_dicts[comparison_type]['2nd value']
-
-            filtered_database = self.yield_ratio[type_dicts[comparison_type]['1st col'] > type_dicts[comparison_type]['2nd col']]
-
-        elif comparator == 'Less than':
-            self.boolean_comp = type_dicts[comparison_type]['1st value'] < type_dicts[comparison_type]['2nd value']
-
-            filtered_database = self.yield_ratio[type_dicts[comparison_type]['1st col'] < type_dicts[comparison_type]['2nd col']]
-
-        else:
-            return "Check comparator value, should be either p greater or lower"
-
+            filtered_database = self.yielddiff_df[
+                (self.yielddiff_df[selection_dict[comparison_type]["db values"]] > comparison_value_lower) &
+                (self.yielddiff_df[selection_dict[comparison_type]["db values"]].shift(1) <= comparison_value_lower)
+            ]
+        elif comparator == 'Lower Than':
+            filtered_database = self.yielddiff_df[
+                (self.yielddiff_df[selection_dict[comparison_type]["db values"]] < comparison_value_lower) &
+                (self.yielddiff_df[selection_dict[comparison_type]["db values"]].shift(1) >= comparison_value_lower)
+            ]
+        elif comparator == 'Between':
+            filtered_database = self.yielddiff_df[
+                (self.yielddiff_df[selection_dict[comparison_type]["db values"]].between(comparison_value_lower, comparison_value_higher)) &
+                ~ (self.yielddiff_df[selection_dict[comparison_type]["db values"]].shift(1).between(comparison_value_lower, comparison_value_higher))
+            ]
         sp500.append(filtered_database)
         ndx.append(filtered_database)
-        rus2k.append(filtered_database)        
-
+        rus2k.append(filtered_database)
+        print(filtered_database)
+        
         return sp500, ndx, rus2k
